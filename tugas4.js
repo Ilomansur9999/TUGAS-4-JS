2a23c14cb39c9ad14f600bdec639bab8e2b4e8ec
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 15:01:05 +0700

    TUGAS 4 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas4.js
@@ -0,0 +1,39 @@
+var joni = 170;
+var jeni = 160;
+var juni = 155;
+
+if(joni>jeni){
+	if(joni>juni){
+		console.log("Tertinggi");
+	}else{
+		console.log("Kedua")
+	}
+}else{
+	if(joni<juni){
+		console.log("Terpendek");
+	}else{
+		console.log("Kedua")
+
+	}
+}
+
+
+// C:\Users\USER\Documents\JS\javascript>node tugas4.js
+// Tertinggi
+
+
+
+
+// function jalansatu() {
+// 	var nilai = 2;
+// 	if(nilai === 5){
+// 		console.log("Tertinggi")
+// 	}else if(nilai === 2){
+// 		console.log("Sedang")
+// 	}
+// 	else{
+// 		console.log("Terpendek")
+// 	}
+// }
+
+// jalansatu()
